7f85ea0d3905ad337590b0c55d25da0da51ca20c
Mimi <mimitraugott@gmail.com>
2020-01-20 14:27:44 -0700

    fixed issues with zipcode selection

--- db/profile queries/zipcode_selection.sql
+++ db/profile queries/zipcode_selection.sql
@@ -1,5 +1,6 @@
 select them.* from users_profile them
 join users_profile me on me.users_zipcode = them.users_zipcode 
 and me.users_zipcode_ext = them.users_zipcode_ext
+and me.users_profile_id = $1
 
 
