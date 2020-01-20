8e716a2a49cde1c8d7fc1f821ff206543de8fc19
Mimi <mimitraugott@gmail.com>
2020-01-20 15:48:48 -0700

    commit after refactoring db queries

--- db/profile queries/gender_selection.sql
+++ db/profile queries/gender_selection.sql
@@ -1,5 +1,4 @@
-select them.* from users_profile them
-join users_profile me on ((me.users_gender_male <> them.users_gender_male and me.users_gender_preference_standard) or
-(me.users_gender_male = them.users_gender_male and me.users_gender_preference_standard = false) and them.users_profile_id <> $1)
-and me.users_profile_id = $1
+select * from users_profile
+where users_gender_male <> $1
+and users_profile_id <> $2
 
