a026e90b6bcdfbf30ca7cbd546e651884951bc81
Mimi <mimitraugott@gmail.com>
2020-01-20 13:55:40 -0700

    commit before starting zipcode selection db query

--- db/profile queries/age_selection.sql
+++ db/profile queries/age_selection.sql
@@ -1,3 +1,8 @@
-select * from users_profile
-where users_age between users_age_preference_min and users_age_preference_max
-and users_profile_id <> $1
+select them.* from users_profile them
+join users_profile me on (them.users_age between me.users_age_preference_min and me.users_age_preference_max)
+and me.users_profile_id = $1
+
+
+-- select * from users_profile
+-- where users_age between users_age_preference_min and users_age_preference_max
+-- and users_profile_id <> $1
