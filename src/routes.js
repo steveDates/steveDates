e2f070e2c94064e6ed6049705bc1667e44dbc809
Mac <mac@MacBook-Air.local>
2020-01-20 11:51:05 -0700

    Added functionality to the register page

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import {Switch, Route} from 'react-router-dom'
 //PAGES
 import Login from './Components/Login/Login'
 import Register from './Components/Register/Register'
+import Swipe from './Components/Swipe/Swipe'
 
 export default (
 <Switch>
     <Route component={Login} exact path='/'/>
     <Route component={Register} exact path='/register'/>
+    <Route component={Swipe} path = '/swipe'/>
     {/* <Route component={Login} exact path='/'/>
     <Route component={Login} exact path='/'/>
     <Route component={Login} exact path='/'/>
