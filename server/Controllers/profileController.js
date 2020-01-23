3110e4c60795b6d69b38c614df4e0f5384b763b1
Mimi <mimitraugott@gmail.com>
2020-01-23 10:00:34 -0700

    getpotentialsbyzip almost complete

--- server/Controllers/profileController.js
+++ server/Controllers/profileController.js
@@ -8,7 +8,7 @@ module.exports = {
       let db = req.app.get('db')
       let data = []
       let newData
-    const { users_zipcode, users_preference_proximity_max } = req.session.user;
+    const { users_id, users_zipcode, users_preference_proximity_max, users_gender_male, users_age, users_age_preference_min, users_age_preference_max, users_gender_preference_standard} = req.session.user;
     try {
         console.log(users_zipcode, users_preference_proximity_max)
       const zipData = await axios({
@@ -19,17 +19,21 @@ module.exports = {
           "x-rapidapi-key": RAPID_API_KEY
         }
       });
-      console.log('zipdata', zipData)
+    //   console.log('zipdata', zipData)
       data = zipData.data.zip_codes
       newData = data.map(el => el.zip_code)
-      let matches = await db.users_profile_activities.find({users_zipcode: newData})
-      res.status(200).send({matches, data});
-      console.log(matches)
+      let matches = await db.users_profile.find({users_zipcode: newData, "users_id <>": users_id })
+    //   console.log('matches', matches)
+      let bestMatches = matches.filter((el,i) => {
+          return el.users_gender_male === users_gender_preference_standard && el.users_gender_preference_standard === users_gender_male && el.users_age >= users_age_preference_min && el.users_age <= users_age_preference_max
+      })
+    //   console.log('bestMatches', bestMatches)
+      res.status(200).send(bestMatches);
     }
     catch(err){
         res.status(500).send(err)
     }
-    console.log("data", newData)
+    // console.log("data", newData)
   }
 };
 //iterate over the zipData and have it return just the zipcode, then either individually insert into the zipcode table or find massives way to do a bulk insert into the zipcode table
